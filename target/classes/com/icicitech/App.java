2db5d6dbeb965399defcb7b3123badf3f3358eb1
gandisaikiran <gandi.saikiran@gmail.com>
2023-02-14 09:34:08 -0800

    secoundcommit

---
+++ target/classes/com/icicitech/App.java
@@ -0,0 +1,14 @@
+Package com.icicitech;
+
+/**
+ *
+ * Hello world!
+ *
+ */
+public class App
+{
+     public static void main( string[] args )
+     {
+        system.out.println( "Hello World!" );
+      }
+}
---
+++ target/classes/com/icicitech/Sample.java
@@ -0,0 +1,14 @@
+Package com.icicitech;
+
+/**
+ *
+ * Hello world!
+ *
+ */
+public class Sample
+{
+     public static void main( string[] args )
+     {
+        system.out.println( "Hello India!" );
+      }
+}
